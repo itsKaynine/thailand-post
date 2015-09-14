e840945a76a4ec6b04f658f2643b4d5d8cb086c7
Kaynine <me@itskaynine.com>
2015-09-14 22:31:05 +0700

    Implement wrapper methods for getItem(s)

--- demo.js
+++ demo.js
@@ -2,28 +2,12 @@
 
 var TrackService = require('./');
 
-var ts = new TrackService(function(err, client) {
-	var getItemsArg = {
-		user: ts.constants.user,
-		password: ts.constants.password,
-		lang: ts.constants.lang,
-		Barcodes: "EN331755897TH"
-	};
-
-  	client.GetItemsJson(getItemsArg, function(err, result) {
+new TrackService().init(function(err, service) {
+	service.getItem("EN331755897TH", function(err, result) {
 		if (err) {
 			return console.log(err);
 		}
 
-		var jsonResult = JSON.parse(result.GetItemsJsonResult);
-  		console.log(jsonResult);
-  	});
-
-  	client.GetItems(getItemsArg, function(err, result) {
-		if (err) {
-			return console.log(err);
-		}
-
-		console.log(result.GetItemsResult.ItemsData.Items);
-  	});
+		console.log(result);
+	});
 });
