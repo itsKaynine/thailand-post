b72e285363694e35661acc5efaac559d7a413726
Kaynine <me@itskaynine.com>
2015-09-15 02:04:34 +0700

    Implement SmartPhoneGetPostLocationJson as searchLocation

--- demo.js
+++ demo.js
@@ -42,4 +42,20 @@ new TrackService().init(function(err, serv) {
 
 		console.log(result);
 	});
+
+	serv.getRates("TH", "1", 0.05, function(err, result) {
+		if (err) {
+			return console.log(err);
+		}
+
+		console.log(result);
+	});
+
+	serv.searchLocation("คลอง", function(err, result) {
+		if (err) {
+			return console.log(err);
+		}
+
+		console.log(result);
+	});
 });
